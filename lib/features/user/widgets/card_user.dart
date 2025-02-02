e28dfe5d4550a8b9873b5b9834bb6cf28df6f2ef
amin75t <167482540+amin75t-work@users.noreply.github.com>
2025-02-02 14:21:38 +0330

    FIXED:add permission.INTERNET to AndroidManifest.xml

--- lib/features/user/widgets/card_user.dart
+++ lib/features/user/widgets/card_user.dart
@@ -40,7 +40,7 @@ class UserCard extends StatelessWidget {
                 children: [
                   Icon(Icons.email),
                   SizedBox(width: 5),
-                  Text(email),
+                  Text(email.substring(10)),
                 ],
               ),
             ],
