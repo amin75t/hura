12bd19a1c46b2ab999413d5d9d2d138193de8766
amin75t <167482540+amin75t-work@users.noreply.github.com>
2025-02-02 13:11:31 +0330

    UPDATE: update the user model

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -2,14 +2,23 @@ class User {
   final int id;
   final String firstName;
   final String lastName;
+  final String avatar;
+  final String email;
 
-  User({required this.id, required this.firstName, required this.lastName});
+  User(
+      {required this.avatar,
+      required this.email,
+      required this.id,
+      required this.firstName,
+      required this.lastName});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
       firstName: json['first_name'],
       lastName: json['last_name'],
+      avatar: json['avatar'],
+      email: json['email'],
     );
   }
 }
